dd0dc129e1d05f638b03a68a1f19abe2303cb0ec
danielopez1007 <pordonezl@miumg.edu.gt>
2026-04-02 13:32:56 -0600

    creacion de codigo

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        metodos m = new metodos();
+        m.Nuevo_totito();
+    }
+}
---
+++ metodos.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class metodos {
+private Scanner K;
+private String [] jugadores;
+private String [][] tablero;
+public metodos(){
+    K=new Scanner(System.in);
+    jugadores= new String[2];
+    tablero= new String[10][10];
+
+}
+public void Nuevo_totito(){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            tablero[i][j]="";
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            System.out.print(tablero[i][j] +"_"+ "│");
+        }
+        System.out.println();
+    }
+}
+}
