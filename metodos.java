e0feee57b7ba94b91800a8e0ae840d2aecaad945
danielopez1007 <pordonezl@miumg.edu.gt>
2026-04-07 22:58:59 -0600

    creacion de  verificacion vertical

--- metodos.java
+++ metodos.java
@@ -26,6 +26,10 @@ public void Nuevo_totito(){
         System.out.println();
     }
 }
+
+    
+
+
 public void Ingresar_jugadores(){
     for(int i=0;i<2;i++){
         System.out.print("Ingrese el nombre del jugador  "+(i+1)+" :");
@@ -37,8 +41,9 @@ public void Ingresar_jugadores(){
 public void Ingresar_jugada() {
     int fila, columna;
     int turno = 0;
+    boolean ganador=false;
     try {
-        while (true) {
+        while (ganador==false) {
             int i = turno % 2;
             System.out.println("Turno del jugador " + jugadores[i]);
 
@@ -49,19 +54,19 @@ public void Ingresar_jugada() {
                 System.out.print("Ingrese la columna donde desea colocar su simbolo: ");
                 columna = K.nextInt();
 
-                // Validar límites
+                
                 if (fila < 0 || fila >= tablero.length || columna < 0 || columna >= tablero[0].length) {
                     System.out.println("Posición inválida. Intente de nuevo.");
                     continue;
                 }
 
-                // Validar si está ocupado
+               
                 if (!tablero[fila][columna].equals(" ")) {
                     System.out.println("Esa casilla ya está ocupada. Intente otra.");
                     continue;
                 }
 
-                // Jugada válida, colocar el símbolo
+            
                 tablero[fila][columna] = simbolos[i];
                 break;
             }
@@ -71,14 +76,31 @@ public void Ingresar_jugada() {
                     System.out.print(tablero[r][c] + "_" + "│");
                 }
                 System.out.println();
+                
+        }
+        int contador =0;
+                    for (int e =0;e<10;e++){
+                    for(int j=0;j<10;j++){
+                        if(tablero[e][j].equals(simbolos[i])){
+                contador++;
+            }else{
+                contador=0;
+            }
+            if(contador==3){
+                System.out.println("El jugador "+jugadores[i]+" ha ganado");
+                ganador=true;
+                break;
+            }
+        }
+    }
+        turno++;
 
             }
-            turno++;
-        }
+            
         
-    } catch (Exception e) {
+        }
+    catch (Exception e) {
         System.out.println("Entrada inválida. Intente de nuevo.");
         K.nextLine();
     }
-}
-    }
+}}
